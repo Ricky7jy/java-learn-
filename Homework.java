8c977352fbaf97c2671744f89e2a2ccc44c42e94
lj1025676027 <35157608+lj1025676027@users.noreply.github.com>
2019-03-29 10:06:54 +0800

    Update Homework.java

--- Homework.java
+++ Homework.java
@@ -10,63 +10,66 @@ public class Homework {
 		int month = sc.nextInt();
 		System.out.print("please input date:");
 		int date = sc.nextInt();
-		if((a % 4 == 0 && a % 100 !=0) || (a % 400 == 0)){
-			if(month == 1) {
-				days = date;
-			}else if(month == 2 ) {
-				days = (month-1) * 31 +date;
-			}else if(month == 3 ) {
-				days = (month-2) * 31 +date + 29;
-			}else if(month == 4 ) {
-				days = (month-2) * 31 +date + 29;
-			}else if(month == 5 ) {
-				days = (month-3) * 31 +date + 29 + 30;
-			}else if(month == 6 ) {
-				days = (month-3) * 31 +date + 29 + 30 * 2;
-			}else if(month == 7 ) {
-				days = (month-4) * 31 +date + 29 + 30 * 2;
-			}else if(month == 8 ) {
-				days = (month-4) * 31 +date + 29 + 30 * 2;
-			}else if(month == 9 ) {
-				days = (month-4) * 31 +date + 29 + 30 * 2;
-			}else if(month == 10 ) {
-				days = (month-4) * 31 +date + 29 + 30 * 3;
-			}else if(month == 11 ) {
-				days = (month-5) * 31 +date + 29 + 30 * 3;
+		if((month < 12 && month > 0)||date < 31 && date > 0) {
+			if((a % 4 == 0 && a % 100 !=0) || (a % 400 == 0)){
+				if(month == 1) {
+					days = date;
+				}else if(month == 2 ) {
+					days = (month-1) * 31 +date;
+				}else if(month == 3 ) {
+					days = (month-2) * 31 +date + 29;
+				}else if(month == 4 ) {
+					days = (month-2) * 31 +date + 29;
+				}else if(month == 5 ) {
+					days = (month-3) * 31 +date + 29 + 30;
+				}else if(month == 6 ) {
+					days = (month-3) * 31 +date + 29 + 30 * 2;
+				}else if(month == 7 ) {
+					days = (month-4) * 31 +date + 29 + 30 * 2;
+				}else if(month == 8 ) {
+					days = (month-4) * 31 +date + 29 + 30 * 2;
+				}else if(month == 9 ) {
+					days = (month-4) * 31 +date + 29 + 30 * 2;
+				}else if(month == 10 ) {
+					days = (month-4) * 31 +date + 29 + 30 * 3;
+				}else if(month == 11 ) {
+					days = (month-5) * 31 +date + 29 + 30 * 3;
+				}else{
+					days = (month-6) * 31 +date + 29 + 30 * 4;
+				}
+				System.out.print("all days is " + days);
 			}else {
-				days = (month-6) * 31 +date + 29 + 30 * 4;
+				if(month == 1) {
+					days = date;
+				}else if(month == 2 ) {
+					days = (month-1) * 31 +date;
+				}else if(month == 3 ) {
+					days = (month-2) * 31 +date + 28;
+				}else if(month == 4 ) {
+					days = (month-2) * 31 +date + 28;
+				}else if(month == 5 ) {
+					days = (month-3) * 31 +date + 28 + 30;
+				}else if(month == 6 ) {
+					days = (month-3) * 31 +date + 28 + 30 * 2;
+				}else if(month == 7 ) {
+					days = (month-4) * 31 +date + 28 + 30 * 2;
+				}else if(month == 8 ) {
+					days = (month-4) * 31 +date + 28 + 30 * 2;
+				}else if(month == 9 ) {
+					days = (month-4) * 31 +date + 28 + 30 * 2;
+				}else if(month == 10 ) {
+					days = (month-4) * 31 +date + 28 + 30 * 3;
+				}else if(month == 11 ) {
+					days = (month-5) * 31 +date + 28 + 30 * 3;
+				}else {
+					days = (month-6) * 31 +date + 28 + 30 * 4;
+				}
+				System.out.print("all days is " + days);
 			}
-			System.out.print("all days is " + days);
 		}else {
-			if(month == 1) {
-				days = date;
-			}else if(month == 2 ) {
-				days = (month-1) * 31 +date;
-			}else if(month == 3 ) {
-				days = (month-2) * 31 +date + 28;
-			}else if(month == 4 ) {
-				days = (month-2) * 31 +date + 28;
-			}else if(month == 5 ) {
-				days = (month-3) * 31 +date + 28 + 30;
-			}else if(month == 6 ) {
-				days = (month-3) * 31 +date + 28 + 30 * 2;
-			}else if(month == 7 ) {
-				days = (month-4) * 31 +date + 28 + 30 * 2;
-			}else if(month == 8 ) {
-				days = (month-4) * 31 +date + 28 + 30 * 2;
-			}else if(month == 9 ) {
-				days = (month-4) * 31 +date + 28 + 30 * 2;
-			}else if(month == 10 ) {
-				days = (month-4) * 31 +date + 28 + 30 * 3;
-			}else if(month == 11 ) {
-				days = (month-5) * 31 +date + 28 + 30 * 3;
-			}else {
-				days = (month-6) * 31 +date + 28 + 30 * 4;
-			}
-			System.out.print("all days is " + days);
+			System.out.print("it is a error day");
 		}
 	}
-	
-	
-	
 }
+	
+	
